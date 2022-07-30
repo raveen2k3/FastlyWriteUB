d42fd0cff7631749fed4cc7b321212cb0ed2c022
Daniel <61970262+rozari0@users.noreply.github.com>
2022-07-30 10:28:59 +0000

    refactor code and we don't need testers anymore. :p

--- bot.py
+++ bot.py
@@ -1,23 +1,28 @@
-from pyrogram import *
+from os import remove
+from sys import version_info
+from time import sleep
+
 import pytesseract
 from PIL import Image
-from os import remove
-from pyrogram.types import Message
-from time import sleep
-from sys import version_info
+from pyrogram import Client
 from pyrogram import __version__ as pyro_version
-from config import API_HASH, API_ID, SESSION_NAME, CHATS, TESTERS
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import API_HASH, API_ID, CHATS, SESSION_NAME
 
 fastly = Client(
-    name="userbot", session_string=SESSION_NAME, api_id=API_ID, api_hash=API_HASH
+    name="userbot",
+    session_string=SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
 )
 
 python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 
 BOT_VERSION = "0.1"
 alive_msg = f"""
-**fastlyWrite USERBOT is Alive & Running**
-    
+**fastlyWrite USERBOT is Alive & Running**\n
     **Python Version:** `{python_version}`
     **Pyrogram Version:** `{pyro_version}`
     **Bot Version:** `{BOT_VERSION}`
@@ -31,19 +36,18 @@ prefixes = [".", "!"]
     & filters.chat(CHATS)
     & filters.user(users=[1877720720, 1983714367, 5053950120])
 )
-async def ping(Client: fastly, message: Message):
+async def ping(_, message: Message):
     path = await fastly.download_media(message, file_name="bot_img.jpg")
     print("Now working")
     sleep(1)
-    slicer = pytesseract.image_to_string(Image.open(f"./downloads/bot_img.jpg"))
+    slicer = pytesseract.image_to_string(Image.open("./downloads/bot_img.jpg"))
     word_to_send = slicer.replace("By @FastlyWriteBot", " ")
     await message.reply_text(word_to_send)
     remove(path)
 
 
-@fastly.on_message(filters.command(["alive"], prefixes) & filters.chat(TESTERS))
-async def alive(Client: fastly, message: Message):
-    user_id = message.from_user.id
+@fastly.on_message(filters.command(["alive"], prefixes) & filters.me)
+async def alive(_, message: Message):
     chat_id = message.chat.id
     await message.reply_text(alive_msg)
     await fastly.delete_messages(chat_id, message.id)
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os import getenv, path
+
 from dotenv import load_dotenv
 
 if path.exists("config.env"):
@@ -8,9 +9,5 @@ if path.exists("config.env"):
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 SESSION_NAME = getenv("SESSION_NAME")
-CHATS = getenv("AUTHORIZED_CHATS", None)
-if CHATS:
+if CHATS := getenv("AUTHORIZED_CHATS", None):
     CHATS = CHATS.split(" ")
-TESTERS = getenv("TESTERS", None)
-if TESTERS:
-    TESTERS = TESTERS.split(" ")
